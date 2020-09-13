c02d8374572ed9fde98bcb093f78050772a315d7
Sawyer Shepherd <contact@sawyershepherd.org>
2020-09-13 04:37:44 -0400

    switched to image library

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ const RESOLUTION: u32 = 100;
 const MAXIMUM_ITERATIONS: u32 = 20;
 const EXPLOSION_THRESHOLD: u32 = 4;
 
-fn mandelbrot(real: f32, imag: f32, max_iter: u32) -> u32 {
+fn mandelbrot(real: f32, imag: f32, max_iter: u32) -> f32 {
     let c = Complex { re: real, im: imag };
     let mut z = Complex { re: 0.0, im: 0.0 };
 
@@ -16,10 +16,25 @@ fn mandelbrot(real: f32, imag: f32, max_iter: u32) -> u32 {
     while i <= max_iter as u32 {
         z = (z * z) + c;
         if z.norm() > (EXPLOSION_THRESHOLD as f32) {
-            return 1;
+            return (i as f32) / (max_iter as f32);
         }
+        i += 1;
     }
-    return 0;
+    return 0.0;
 }
 
-fn main() {}
+fn main() {
+    let mut image_buffer = image::ImageBuffer::new(RESOLUTION, RESOLUTION);
+    for y_pixel in 0..RESOLUTION {
+        for x_pixel in 0..RESOLUTION {
+            let x_real = 4.0 / (RESOLUTION as f32) - 2.0;
+            let y_imag = -4.0 / (RESOLUTION as f32) + 2.0;
+
+            let color_multiplier = mandelbrot(x_real, y_imag, MAXIMUM_ITERATIONS);
+            let rgb_value = (255.0 * color_multiplier) as u8;
+            let pixel = image_buffer.get_pixel_mut(x_pixel, y_pixel);
+            *pixel = image::Rgb([rgb_value, rgb_value, rgb_value]);
+        }
+    }
+    image_buffer.save("fractal.png").unwrap();
+}
