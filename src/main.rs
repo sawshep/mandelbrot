d1b567a41bdfc41dbec11352e4793a4a95a78086
Sawyer Shepherd <contact@sawyershepherd.org>
2020-09-14 20:23:27 -0400

    Proper rendering

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,26 @@ extern crate num_complex;
 
 use image::ImageBuffer;
 use num_complex::Complex;
-use std::io::BufRead::read_line;
-//use std::thread;
+use std::io::{stdin, stdout, Write};
 
-const RESOLUTION: u32 = 1000;
-
-const MINIMUM_REAL: f32 = -2.0;
-const MAXIMUM_REAL: f32 = 2.0;
-const MINIMUM_IMAGINARY: f32 = -2.0;
-const MAXIMUM_IMAGINARY: f32 = 2.0;
-
-const X_SCALE: f32 = (MAXIMUM_REAL - MINIMUM_REAL) / ((RESOLUTION - 1) as f32);
-const Y_SCALE: f32 = (MAXIMUM_IMAGINARY - MINIMUM_IMAGINARY) / ((RESOLUTION - 1) as f32);
-const MAXIMUM_ITERATIONS: u32 = 100;
-const EXPLOSION_THRESHOLD: u32 = 2;
-
-fn mandelbrot(real: f32, imag: f32, max_iter: u32) -> f32 {
+/**
+ * The Mandelbrot Set is the set of
+ * `C` in `Z_n+1 = (Z_n)^2 + C`
+ * where `Z` does not diverge to infinity
+ * (In this case, a threshold)
+ * after a number of iterations.
+ * `Z` always starts at zero for every value of `C`.
+ */
+/// The return value (the number of iterations
+/// before `Z` diverges over `threshold`)
+/// is designed to be used as a multiplier for RGB values.
+fn mandelbrot(real: f32, imag: f32, threshold: f32, max_iter: u32) -> f32 {
     let c = Complex { re: real, im: imag };
     let mut z = Complex { re: 0.0, im: 0.0 };
 
     let mut i: u32 = 0;
     while i <= max_iter as u32 {
-        if z.norm() > (EXPLOSION_THRESHOLD as f32) {
+        if z.norm() > (threshold as f32) {
             return (i as f32) / (max_iter as f32);
         }
         z = (z * z) + c;
@@ -33,36 +31,62 @@ fn mandelbrot(real: f32, imag: f32, max_iter: u32) -> f32 {
     return 1.0;
 }
 
-fn main() {
-    let resolution_input = String::new();
-    loop{
-        let resolution: u32 = match read_line(&mut resolution_input).unwrap() {
-            Ok(n) => {
-                match resolution_input.parse::<u32>() {
-                    Ok(n) => n,
-                    Err(_) => println!("Retry"),
-                }
-            }
-            Err(e) => println!("Big error"),
+/// Parses the user input of a `prompt`
+/// to a type provided in the turbofish.
+fn prompt_to_num<T: std::str::FromStr>(prompt: &str) -> T {
+    loop {
+        print!("{} ", prompt);
+        stdout().flush().expect("Failed to flush stdout buffer");
+
+        let mut input = String::new();
+        stdin()
+            .read_line(&mut input)
+            .expect("Failed to read from stdin");
+        match input.trim().parse::<T>() {
+            Ok(n) => return n,
+            Err(_) => println!("Invalid input"),
         }
     }
 }
 
+fn main() {
+    let pixel_res = prompt_to_num::<u32>("Image side length:");
+    let coord_center_x = prompt_to_num::<f32>("X center of coordinate sector:");
+    let coord_center_y = prompt_to_num::<f32>("Y center of coordinate sector:");
+    let coord_res = prompt_to_num::<f32>("Side length of coordinate sector:");
+    let max_iter = prompt_to_num::<u32>("Maximum iterations:");
+    let threshold = prompt_to_num::<f32>("Infinity threshold:");
 
-    let mut image_buffer = ImageBuffer::new(RESOLUTION, RESOLUTION);
-    println!("Image buffer created");
+    println!("Initializing variables...");
+    let coord_min_x = coord_center_x - coord_res / 2.0;
+    let coord_max_x = coord_center_x + coord_res / 2.0;
+    let coord_min_y = coord_center_y - coord_res / 2.0;
+    let coord_max_y = coord_center_y + coord_res / 2.0;
+
+    let x_scale = (coord_max_x - coord_min_x) / pixel_res as f32;
+    // Due to the way a pixel coordinate system
+    // differs from a traditional system,
+    // `y_scale` must be negative for fractals
+    // that are NOT symmetrical about
+    // the real axis to render properly.
+    // This does not include the Mandelbrot Set
+    let y_scale = (coord_max_y - coord_min_y) / pixel_res as f32;
+
+    // TODO: To save memory, first save a blank
+    // image to secondary storage, and then create
+    // individual buffers for every `pixel_y`.
+    let mut image_buffer = ImageBuffer::new(pixel_res, pixel_res);
+
     println!("Running...");
     for (pixel_x, pixel_y, pixel) in image_buffer.enumerate_pixels_mut() {
-        //let child = thread::spawn(|| {
-        let x_real = MINIMUM_REAL + (pixel_x as f32) * X_SCALE;
-        let y_imag = -1.0 * (MINIMUM_IMAGINARY + (pixel_y as f32) * Y_SCALE);
+        let x_real = coord_min_x + (pixel_x as f32) * x_scale;
+        let y_imag = coord_min_y + (pixel_y as f32) * y_scale;
 
-        let color_multiplier = mandelbrot(x_real, y_imag, MAXIMUM_ITERATIONS);
+        let color_multiplier = mandelbrot(x_real, y_imag, threshold, max_iter);
         let rgb_value = (255.0 * color_multiplier) as u8;
         *pixel = image::Rgb([rgb_value, rgb_value, rgb_value]);
-        //});
     }
-    println!("Saving image buffer to fractal.png...");
+    println!("Saving image to fractal.png...");
     image_buffer.save("fractal.png").unwrap();
     println!("Done");
 }
